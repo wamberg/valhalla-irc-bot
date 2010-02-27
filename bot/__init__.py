42fc7e0fffd70dfac82e49d6ec5182a5be3b70c7
wamberg <wamberg@technushi.com>
2010-02-27 16:48:37 -0500

    Added 'tiny:' command that searches the message for a URL and tinyurlifies it.

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,12 +13,14 @@ Parameters
     <valhalla_pass> - Password for valhalla basic authentication
 """
 from datetime import datetime
-import sys
-import urllib2
+import re, sys, threading, urllib2
 
 from twisted.words.protocols import irc
 from twisted.internet import protocol, reactor
-from django.utils import simplejson
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 
 def install_opener(uri, user, password):
@@ -44,10 +46,9 @@ class ValhallaBot(irc.IRCClient):
     def joined(self, channel):
         print "Joined %s." % (channel,)
 
-    def _msg_to_deed_json(self, user, msg):
+    def _msg_to_deed_json(self, speaker, msg):
         deed, msg = self._process_commands(msg)
         if not msg: return None
-        speaker = user.split("!", 1)[0]
         deed_date = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
         # deed must be a list of one
         deed.update({'pk': 1,
@@ -59,13 +60,17 @@ class ValhallaBot(irc.IRCClient):
                     'user': 1
                  }
                })
-        return simplejson.dumps([deed])
+        return json.dumps([deed])
 
     def _process_commands(self, msg):
         if msg.startswith('twitter:'):
             return {'dispatch': ['twitter']}, msg[8:].strip()
         elif msg.startswith('otr:'):
             return {}, None
+        elif msg.startswith('tiny:'):
+            url_match = self.url_re.search(msg)
+            if url_match:
+                TinyURLThread(self, url_match.group(0)).start()
         return {}, msg
 
     def privmsg(self, user, channel, msg):
@@ -73,7 +78,11 @@ class ValhallaBot(irc.IRCClient):
         POST message to django-vallhalla API using basic authentication.
         """
         if user and user.rfind('!') > 0:
-            deed_json = self._msg_to_deed_json(user, msg)
+            deed_json = None
+            speaker = user.split("!", 1)[0]
+            if speaker != self.nickname:
+                deed_json = self._msg_to_deed_json(speaker, msg)
+            # have the bot take actions based on the content of a msg
             if not deed_json: return None
             request = urllib2.Request('http://' + self.valhalla_uri, deed_json)
             request.add_header('Content-Type', 'application/json')
@@ -92,6 +101,7 @@ class ValhallaBotFactory(protocol.ClientFactory):
         self.nickname = nickname
         self.valhalla_uri = uri
         self.opener = install_opener('http://' + uri, user, password)
+        self.url_re = re.compile("(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?") 
 
     def buildProtocol(self, addr):
         """
@@ -101,6 +111,7 @@ class ValhallaBotFactory(protocol.ClientFactory):
         p.factory = self
         p.valhalla_uri = self.valhalla_uri
         p.opener = self.opener
+        p.url_re = self.url_re
         return p
 
     def clientConnectionLost(self, connector, reason):
@@ -109,6 +120,19 @@ class ValhallaBotFactory(protocol.ClientFactory):
 
     def clientConnectionFailed(self, connector, reason):
         print "Could not connect: %s" % (reason,)
+
+
+class TinyURLThread(threading.Thread):
+    def __init__(self, bot, url):
+        self.bot = bot
+        self.url = url
+        threading.Thread.__init__(self)
+
+    def run(self):
+        tiny_url = urllib2.urlopen(
+                'http://tinyurl.com/api-create.php?url=%s' % self.url)
+        self.bot.say(self.bot.factory.channel, tiny_url.read())
+
 
 
 if __name__ == "__main__":
